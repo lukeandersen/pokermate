be82b5e429dd1304ce2815b3e84a2b952e721e06
Luke Andersen <luke@lawebdesign.com.au>
2020-05-13 12:12:24 +1000

    Add env vars for build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@ module.exports = {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         // Add the new plugin to the existing webpack plugins
         config.plugins.push(new Dotenv({ silent: true }))
-
         return config
+    },
+    env: {
+        FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+        FIREBASE_AUTHDOMAIN: process.env.FIREBASE_AUTHDOMAIN,
+        FIREBASE_DATABASEURL: process.env.FIREBASE_DATABASEURL,
+        FIREBASE_PROJECTID: process.env.FIREBASE_PROJECTID,
+        FIREBASE_STORAGEBUCKET: process.env.FIREBASE_STORAGEBUCKET,
+        FIREBASE_MESSAGINGSENDERID: process.env.FIREBASE_MESSAGINGSENDERID,
+        FIREBASE_APPID: process.env.FIREBASE_APPID,
+        FIREBASE_MEASUREMENTID: process.env.FIREBASE_MEASUREMENTID,
     },
 }
