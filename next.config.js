280be995c4840dcac29eb123383aa139b5c588d6
Luke Andersen <luke@lawebdesign.com.au>
2020-05-18 11:53:29 +1000

    Update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const Dotenv = require('dotenv-webpack')
 
 module.exports = {
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // Add the new plugin to the existing webpack plugins
+        // Add the new plugin to the existing webpack plugins 👍
         config.plugins.push(new Dotenv({ silent: true }))
         return config
     },
